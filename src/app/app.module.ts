0823ab5fbf9f7f2656ea57f622268b4ccc4fa6fc
imane <ibenzaouagh@gmail.com>
2023-04-05 02:57:37 +0200

    Imane Locals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LocalCreateComponent } from './view/locals/local-create/local-create.component';
+import { LocalListComponent } from './view/locals/local-list/local-list.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {Ng2SearchPipeModule} from "ng2-search-filter";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocalCreateComponent,
+    LocalListComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        FormsModule,
+        Ng2SearchPipeModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/controler/model/local.model.ts
@@ -0,0 +1,12 @@
+export class Local {
+  public ref  : string='';
+  public cin : object | undefined;
+
+public test : object | undefined;
+  public rueCode : string='';
+  public categorieLocalName:string='';
+  public dernierAnneePayee : number = 0;
+  public dernierTrimestrePayee:number=0;
+
+
+}
